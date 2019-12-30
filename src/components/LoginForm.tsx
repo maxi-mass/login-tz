f93cbc0872a299a59d2b17ed06f6f60ab7b5a17a
Maxim Svistunov <svistunovms@lad24.ru>
2019-12-30 11:51:40 +0300

    Added form. In progress.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: 1440px;
+  height: 1024px;
+  background: #ffffff;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  width: 380px;
+  height: 541px;
+  left: 530px;
+  top: 241px;
+  border: 1px solid black;
+`;
+
+export const EnterAccount = styled.div`
+  height: 20px;
+  width: 50px;
+  margin-top: 40px;
+  border: 1px solid black;
+`;
+
+const FormInnerWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 export const LoginForm: React.FC = () => {
-  return <div>LoginForm</div>;
+  return (
+    <Wrapper>
+      <Content>
+        <form>
+          <FormInnerWrap>
+            <div>
+              <input type="text" name="email" />
+            </div>
+            <div>
+              <input type="password" name="password" />
+            </div>
+            <div>
+              <input type="checkbox" name="remember_me" />
+              Запомнить меня
+            </div>
+            <div>
+              <button>Отправить</button>
+            </div>
+          </FormInnerWrap>
+        </form>
+      </Content>
+    </Wrapper>
+  );
 };
